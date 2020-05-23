b3c5e2df1a91d7f5b32d076b879209a335a4cad4
Aditya Bhandari <63658819+ADITYABHNDARI@users.noreply.github.com>
2020-05-23 20:39:03 +0530

    Add files via upload

--- LoveAutomate/honey_love.py
+++ LoveAutomate/honey_love.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
  
-account_sid = 'AC8a6157dcb4a9745b7dfbc0150574448a' 
-auth_token = 'fa6279e45cb537a44d90529a8db3f2eb' 
+account_sid = 'your SID' 
+auth_token = 'YOUR TOKEN FROM TWILIO' 
 client = Client(account_sid, auth_token) 
 def send_love():
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',
-                              body='CHUTIYE AAYA MSG',        
-                              to='whatsapp:+917232074757' 
+                              body='YOUR MSG',        
+                              to='whatsapp: YOUR NO.' 
                           ) 
  
     print(message.sid)
